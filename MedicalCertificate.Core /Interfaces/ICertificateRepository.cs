d6797eb01a048c644b67eab6b78d64867d87ef55
kenessarybaigali <baigal1ik@gmail.com>
2025-06-03 12:22:50 +0500

    solve the problem

--- MedicalCertificate.Core /Interfaces/ICertificateRepository.cs
+++ MedicalCertificate.Core /Interfaces/ICertificateRepository.cs
@@ -1,12 +1,13 @@
 using MedicalCertificate.Core.Entities;
 using MedicalCertificate.Core.Enums;
 
+
 namespace MedicalCertificate.Core.Interfaces;
 
 public interface ICertificateRepository
 {
-    Task AddAsync(MedicalCertificate certificate);
-    Task<MedicalCertificate?> GetByIdAsync(Guid id);
-    Task<IEnumerable<MedicalCertificate>> GetByIinAsync(string iin);
+    Task AddAsync(Entities.MedicalCertificate certificate);
+    Task<Entities.MedicalCertificate?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Entities.MedicalCertificate>> GetByIinAsync(string iin);
     Task UpdateStatusAsync(Guid id, CertificateStatus status, string? reviewerComment = null);
 }
